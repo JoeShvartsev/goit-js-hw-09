ff5f565f3c89d5101f06e9ea73b5d038981300b3
JoeShvartsev <120992324+JoeShvartsev@users.noreply.github.com>
2023-05-12 11:30:34 +0300

    qwd

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,26 @@
+const ref ={
+    startBtn: document.querySelector("button[data-start]"),
+    stopBtn: document.querySelector("button[data-stop]"),
+    bodyEl: document.body
+
+}
+
+const changeColor = () => {
+    ref.startBtn.disabled = true;
+    timerId = setInterval(() => {
+        ref.bodyEl.style.backgroundColor = getRandomHexColor()
+    }, 1000);
+    }
+ref.startBtn.addEventListener('click', changeColor)
+
+const stopChangeColor = () =>{
+    ref.startBtn.disabled = false;
+    ref.stopBtn.addEventListener("click", () => {
+        clearInterval(timerId);
+    });
+}
+ref.stopBtn.addEventListener('click', stopChangeColor)
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
