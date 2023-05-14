6d541e93e3704e83e9564a0be37fbddbbce19241
JoeShvartsev <120992324+JoeShvartsev@users.noreply.github.com>
2023-05-14 12:21:19 +0000

    Deploying to gh-pages from @ JoeShvartsev/goit-js-hw-09@32cefec603c12a58af9d9d37c04bb2239ab64559 🚀

--- 01-color-switcher.974cb572.js
+++
@@ -1,2 +0,0 @@
-!function(){var t={startBtn:document.querySelector("button[data-start]"),stopBtn:document.querySelector("button[data-stop]"),bodyEl:document.body};t.startBtn.addEventListener("click",(function(){t.startBtn.disabled=!0,timerId=setInterval((function(){t.bodyEl.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3)}));t.stopBtn.addEventListener("click",(function(){t.startBtn.disabled=!1,clearInterval(timerId)}))}();
-//# sourceMappingURL=01-color-switcher.974cb572.js.map
