62c1b851f149f7e7274d8c6f26f37e49af380ce7
JoeShvartsev <120992324+JoeShvartsev@users.noreply.github.com>
2023-05-14 12:14:36 +0000

    Deploying to gh-pages from @ JoeShvartsev/goit-js-hw-09@4d8b85526436b0d9d42e9d646beb6898b108c626 🚀

--- 01-color-switcher.28924fc4.js
+++
@@ -1,2 +0,0 @@
-const t={startBtn:document.querySelector("button[data-start]"),stopBtn:document.querySelector("button[data-stop]"),bodyEl:document.body};t.startBtn.addEventListener("click",(()=>{t.startBtn.disabled=!0,timerId=setInterval((()=>{t.bodyEl.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)}));t.stopBtn.addEventListener("click",(()=>{t.startBtn.disabled=!1,t.stopBtn.addEventListener("click",(()=>{clearInterval(timerId)}))}));
-//# sourceMappingURL=01-color-switcher.28924fc4.js.map
